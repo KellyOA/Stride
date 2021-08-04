d6648d4d9b1c5a44ad85730d74b1a15ec6802113
Kelly Olmos <Alexandra120414@gmail.com>
2021-08-04 11:56:45 -0500

    Initial Commit

---
+++ Stride/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Stride
+//
+//  Created by Kelly Olmos on 8/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+//hi
+    //hi
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       // let newLayer = CAGradientLayer()
+           // newLayer.colors =
+                //[UIColor.gray.cgColor, UIColor.cyan.cgColor]
+            //newLayer.frame = view.frame
+            
+            //view.layer.addSublayer(newLayer)
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
